eeab60603c46e7e39af8988fb231622892b540d4
dnispe <denisnem@hotmail.com>
2025-09-17 22:14:33 -0300

    Configurações iniciais do Express e nodemon

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("Olá mundo, Node.js está rodando!");
+});
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
