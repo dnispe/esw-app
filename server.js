5ab0ce615d6c61901e982d8cd9df2811863e1fbf
dnispe <denisnem@hotmail.com>
2025-09-18 20:51:49 -0300

    Configurações iniciais do Express e nodemon

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-    res.send("Olá mundo, Node.js está rodando!");
+// Eventos
+const eventos = [
+    { id: 1, nome: "Zé Ramalho", local: "Parque de Exposições", endereco: "R. Rio Sena, 124 - Santo Antonio I, Pte. Nova - MG, 35430-195" },
+    { id: 2, nome: "Stand-up Comedy", local: "Teatro Central", endereco: "Rua Alegre, 45" },
+    { id: 3, nome: "Concerto Clássico", local: "Sala de Concertos", endereco: "Praça da Cultura, 10" }
+];
+
+// Tela 1: Listagem de eventos
+app.get("/", (req, res) => {
+    res.render("index", { eventos });
 });
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+app.listen(PORT, () => console.log('Servidor rodando em http://localhost:3000'));
